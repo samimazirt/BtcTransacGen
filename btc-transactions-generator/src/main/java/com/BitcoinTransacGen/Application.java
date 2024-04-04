081220d2aeadc18388b8a5cb40a09c58cbd6f2a7
Sami Mazirt <sami.mazirt@epita.fr>
2024-04-04 14:06:52 +0200

    008 sami

--- btc-transactions-generator/src/main/java/com/BitcoinTransacGen/Application.java
+++ btc-transactions-generator/src/main/java/com/BitcoinTransacGen/Application.java
@@ -1,22 +1,19 @@
 package com.BitcoinTransacGen;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.Reader;
 import java.math.BigDecimal;
-import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.logging.Logger;
-import java.net.HttpURLConnection;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import wf.bitcoin.javabitcoindrpcclient.*;
 import wf.bitcoin.javabitcoindrpcclient.BitcoindRpcClient.ExtendedTxInput;
-import wf.bitcoin.javabitcoindrpcclient.BitcoindRpcClient.MultiSig;
 import wf.bitcoin.javabitcoindrpcclient.BitcoindRpcClient.RawTransactionSigningOrVerificationError;
 import wf.bitcoin.javabitcoindrpcclient.BitcoindRpcClient.SignedRawTransaction;
-import wf.bitcoin.javabitcoindrpcclient.BitcoindRpcClient.TxInput;
 import wf.bitcoin.javabitcoindrpcclient.BitcoindRpcClient.Unspent;
 import wf.bitcoin.javabitcoindrpcclient.util.Chain;
 import wf.bitcoin.javabitcoindrpcclient.util.Util;
@@ -55,179 +52,25 @@ public class Application
 		// 2. make sure the bitcoin client is running
 		// 3. make sure it is running on regtest
 
-		signRawTransactionWithKeyTest_P2SH_MultiSig(client);
+		//signRawTransactionWithKeyTest_P2SH_MultiSig(client);
 		signRawTransactionWithKeyTest_P2SH_P2WPKH(client);
 	}
 
-	static void signRawTransactionWithKeyTest_P2SH_MultiSig(BitcoindRpcClient client)
-	{
-		LOGGER.info("=== Testing scenario: signRawTransactionWithKey ( P2SH-multiSigAddr(2-of-2, [addr1, addr2]) -> addr4 )");
 
-		///////////////////////////////////////////
-		// Prepare transaction 1 (addr3 -> multisig)
-		///////////////////////////////////////////
-		LOGGER.info("Preparing tx1 (addr3 -> multisig)");
-
-		JsonRPCClient jsonRpcClient = new JsonRPCClient();
-		String randomWalletName = generateRandomString(10);
-		// Call createWallet function from JsonRPCClient
-		try {
-			Map<String, Object> result = jsonRpcClient.createWallet(randomWalletName);
-			String walletName = (String) result.get("name");
-			String warning = (String) result.get("warning");
-			LOGGER.info("Wallet created: " + walletName);
-			if (warning != null) {
-				LOGGER.warning("Warning: " + warning);
-			}
-		} catch (GenericRpcException e) {
-			LOGGER.severe("Error creating wallet: " + e.getMessage());
-			//return; // Exit the function if an error occurs
-
-			//loading because couldn't create cause already exist
-			try {
-				Map<String, Object> result = jsonRpcClient.loadWallet(randomWalletName);
-				String walletName = (String) result.get("name");
-				String warning = (String) result.get("warning");
-				LOGGER.info("Wallet loaded: " + walletName);
-				if (warning != null) {
-					LOGGER.warning("Warning: " + warning);
-				}
-			} catch (GenericRpcException eLoad) {
-				LOGGER.severe("Error loading wallet: " + eLoad.getMessage());
-				//return; // Exit the function if an error occurs
-			}
-
-
-
-		}
-
-
-		String addr1 = client.getNewAddress();
-		LOGGER.info("Created address addr1: " + addr1);
-
-		String addr2 = client.getNewAddress();
-		LOGGER.info("Created address addr2: " + addr2);
-
-		// Create P2SH multisig address that wallet can sign
-		// Command also adds it to wallet, allowing us to track and spend payments received by that address
-		// See https://bitcoin.stackexchange.com/questions/36053/difference-between-createmultisig-and-addmultisigaddress
-		MultiSig p2shMultiSig = client.addMultiSigAddress(2, Arrays.asList(addr1, addr2));
-		String p2shMultiSigAddr = p2shMultiSig.address();
-		LOGGER.info("Created and added to wallet the P2SH-multiSigAddr(2-of-2, [addr1, addr2]) : " + p2shMultiSigAddr);
-
-		String addr3 = client.getNewAddress();
-		LOGGER.info("Created address addr3: " + addr3);
-
-		List<String> generatedBlocksHashes = client.generateToAddress(110, addr3);
-		LOGGER.info("Generated " + generatedBlocksHashes.size() + " blocks for addr3");
-
-		List<Unspent> availableUtxosForTx1 = client.listUnspent(0, Integer.MAX_VALUE, addr3);
-		LOGGER.info("Found " + availableUtxosForTx1.size() + " UTXOs (unspent transaction outputs) belonging to addr3");
-
-		TxInput selectedUtxoForTx1 = availableUtxosForTx1.get(0);
-		LOGGER.info("Selected UTXO which will be used in tx1 (addr3 -> P2SH-multiSigAddr) : " + selectedUtxoForTx1);
-
-		//set fee ?
-		BigDecimal estimatedFee = BigDecimal.valueOf(0.0000200);
-		client.setTxFee(estimatedFee);
-
-
-		// Fire off transaction 1 (addr3 -> multisig)
-		String tx1ID = client.sendToAddress(p2shMultiSigAddr, selectedUtxoForTx1.amount());
-		LOGGER.info("UTXO sent to P2SH-multiSigAddr, tx1 ID: " + tx1ID);
-
-		///////////////////////////////////////////
-		// Prepare transaction 2 (multisig -> addr4)
-		///////////////////////////////////////////
-		LOGGER.info("Preparing tx2 (multisig -> addr4)");
-
-		String addr4 = client.getNewAddress();
-		LOGGER.info("Created address addr4: " + addr4);
-
-		List<Unspent> availableUtxosForTx2 = client.listUnspent(0, 999, p2shMultiSigAddr);
-		LOGGER.info("Found " + availableUtxosForTx2.size() + " UTXOs (unspent transaction outputs) belonging to P2SH-multiSigAddr");
-
-		Unspent selectedUtxoForTx2 = availableUtxosForTx2.get(0);
-		LOGGER.info("Selected UTXO which will be used in tx2 (P2SH-multiSigAddr -> addr4) : " + selectedUtxoForTx2);
-
-		ExtendedTxInput inputP2SH = new ExtendedTxInput(
-				selectedUtxoForTx2.txid(),
-				selectedUtxoForTx2.vout(),
-				selectedUtxoForTx2.scriptPubKey(),
-				selectedUtxoForTx2.amount(),
-				selectedUtxoForTx2.redeemScript(),
-				selectedUtxoForTx2.witnessScript());
-
-		LOGGER.info("inputP2SH txid: " + 			inputP2SH.txid());
-		LOGGER.info("inputP2SH vout: " + 			inputP2SH.vout());
-		LOGGER.info("inputP2SH scriptPubKey: " + 	inputP2SH.scriptPubKey());
-		LOGGER.info("inputP2SH amount: " + 			inputP2SH.amount());
-
-		BitcoinRawTxBuilder rawTxBuilder = new BitcoinRawTxBuilder(client);
-		rawTxBuilder.in(inputP2SH);
-
-		// Found no other reliable way to estimate the fee in a test
-		// Therefore, setting the fee for this tx 200 satoshis (what appears to be the min relay fee)
-
-		BigDecimal txToAddr4Amount = selectedUtxoForTx2.amount().subtract(estimatedFee);
-		rawTxBuilder.out(addr4, txToAddr4Amount);
-
-		String unsignedRawMultiSigToAddr4TxHex = rawTxBuilder.create();
-		LOGGER.info("Created unsignedRawTx from P2SH-multiSigAddr(2-of-2, [addr1, addr2]) to addr4: " + unsignedRawMultiSigToAddr4TxHex);
-
-		// Sign multi-sig transaction
-		SignedRawTransaction srTx = client.signRawTransactionWithKey(
-				unsignedRawMultiSigToAddr4TxHex,
-				Arrays.asList(client.dumpPrivKey(addr1), client.dumpPrivKey(addr2)), // Using private keys of addr1 and addr2 (multisig 2-of-2)
-				Arrays.asList(inputP2SH),
-				null);
-		LOGGER.info("signedRawTx hex: " + srTx.hex());
-		LOGGER.info("signedRawTx complete: " + srTx.complete());
-
-		List<RawTransactionSigningOrVerificationError> errors = srTx.errors();
-		if (errors != null)
-		{
-			LOGGER.severe("Found errors when signing");
-
-			for (RawTransactionSigningOrVerificationError error : errors)
-			{
-				LOGGER.severe("Error: " + error);
-			}
-		}
-
-		// Transaction 2 : multisig -> addr4
-		String sentRawTransactionID = client.sendRawTransaction(srTx.hex());
-		LOGGER.info("Sent signedRawTx (txID): " + sentRawTransactionID);
-
-		BitcoindRpcClient.Transaction transaction = client.getTransaction(sentRawTransactionID);
-		System.out.println("Transaction details:\n" + prettyPrintJson(transaction.toString()));
-
-		//unload wallet
-		try {
-			Map<String, Object> result = jsonRpcClient.unloadWallet(randomWalletName);
-			String walletName = (String) result.get("name");
-			String warning = (String) result.get("warning");
-			LOGGER.info("Wallet unloaded: " + walletName);
-			if (warning != null) {
-				LOGGER.warning("Warning: " + warning);
-			}
-		} catch (GenericRpcException e) {
-			LOGGER.severe("Error unloading wallet: " + e.getMessage());
-			//return; // Exit the function if an error occurs
-		}
-
-
-	}
 
 	/**
 	 * Signing a transaction to a P2SH-P2WPKH address (Pay-to-Witness-Public-Key-Hash)
+	 *
+	 * @return
 	 */
-	static void signRawTransactionWithKeyTest_P2SH_P2WPKH(BitcoindRpcClient client)
+	static String signRawTransactionWithKeyTest_P2SH_P2WPKH(BitcoindRpcClient client)
 	{
 		LOGGER.info("=== Testing scenario: signRawTransactionWithKey (addr1 -> addr2)");
 		// Call createWallet function from JsonRPCClient
 		JsonRPCClient jsonRpcClient = new JsonRPCClient();
 		String randomWalletName = generateRandomString(10);
+		BigDecimal minAmount = BigDecimal.valueOf(0.0001); // Minimum amount
+		BigDecimal maxAmount = BigDecimal.valueOf(1.0);
 
 		try {
 			Map<String, Object> result = jsonRpcClient.createWallet(randomWalletName);
@@ -265,8 +108,10 @@ public class Application
 		String addr2 = client.getNewAddress();
 		LOGGER.info("Created address addr2: " + addr2);
 
-		List<String> generatedBlocksHashes = client.generateToAddress(110, addr1);
+		List<String> generatedBlocksHashes = client.generateToAddress(510, addr1);
 		LOGGER.info("Generated " + generatedBlocksHashes.size() + " blocks for addr1");
+
+		System.out.println("ballll" + client.getBalance());
 
 		List<Unspent> utxos = client.listUnspent(0, Integer.MAX_VALUE, addr1);
 		LOGGER.info("Found " + utxos.size() + " UTXOs (unspent transaction outputs) belonging to addr1");
@@ -295,7 +140,7 @@ public class Application
 
 		String tx1ID = client.sendToAddress(addr1, selectedUtxo.amount());
 		LOGGER.info("UTXO sent to P2SH-multiSigAddr, tx1 ID: " + tx1ID);
-		//System.out.println(transaction.());
+		System.out.println("mmmmm" + client.getTransaction(tx1ID));
 
 
 		// Found no other reliable way to estimate the fee in a test
@@ -333,9 +178,14 @@ public class Application
 		String sentRawTransactionID = client.sendRawTransaction(srTx.hex());
 		LOGGER.info("Sent signedRawTx (txID): " + sentRawTransactionID);
 
-		BitcoindRpcClient.Transaction transaction = client.getTransaction(sentRawTransactionID);
-		//System.out.println(transaction.());
-		System.out.println("Transaction details:\n" + prettyPrintJson(transaction.toString()));
+		BitcoindRpcClient.RawTransaction rawTransactionDecoded = client.getRawTransaction(jsonRpcClient.decodeRawTransaction(srTx.hex()).vIn().get(0).txid());
+		String transaction = client.getTransaction(sentRawTransactionID).toString().replaceFirst(addr2.toString(), addr1.toString());
+		//System.out.println("transac details"+ prettyPrintJson(transaction));
+		System.out.println("ballll" + client.getBalance());
+		Gson gson = new Gson();
+
+
+
 
 		//unload wallet
 		try {
@@ -350,6 +200,48 @@ public class Application
 			LOGGER.severe("Error unloading wallet: " + e.getMessage());
 			//return; // Exit the function if an error occurs
 		}
+
+		return transaction;
+	}
+
+	public static BigDecimal generateRandomAmount(BigDecimal min, BigDecimal max) {
+		Random random = new Random();
+
+		// Define the distribution of transaction amounts
+		// For simplicity, you can define a distribution based on ranges and their corresponding probabilities
+		// Adjust the probabilities based on your analysis of real-world Bitcoin transaction data
+		double[] rangeProbabilities = {0.1, 0.2, 0.3, 0.2, 0.1, 0.05, 0.03, 0.02};
+
+		// Define the ranges corresponding to the probabilities
+		BigDecimal[] rangeMinimums = {
+				BigDecimal.valueOf(0.0001),
+				BigDecimal.valueOf(0.001),
+				BigDecimal.valueOf(0.01),
+				BigDecimal.valueOf(0.1),
+				BigDecimal.valueOf(1.0),
+				BigDecimal.valueOf(2.0),
+				BigDecimal.valueOf(5.0),
+				BigDecimal.valueOf(10.0)
+		};
+
+		// Select a range based on probabilities
+		double randomNumber = random.nextDouble();
+		double cumulativeProbability = 0.0;
+		int selectedRangeIndex = 0;
+		for (int i = 0; i < rangeProbabilities.length; i++) {
+			cumulativeProbability += rangeProbabilities[i];
+			if (randomNumber <= cumulativeProbability) {
+				selectedRangeIndex = i;
+				break;
+			}
+		}
+
+		// Generate a random value within the selected range
+		BigDecimal selectedRangeMin = rangeMinimums[selectedRangeIndex];
+		BigDecimal selectedRangeMax = (selectedRangeIndex == rangeMinimums.length - 1) ? max : rangeMinimums[selectedRangeIndex + 1];
+		BigDecimal randomBigDecimal = selectedRangeMin.add(new BigDecimal(random.nextDouble()).multiply(selectedRangeMax.subtract(selectedRangeMin)));
+
+		return randomBigDecimal.setScale(8, BigDecimal.ROUND_HALF_UP);
 	}
 
 	private static String prettyPrintJson(String jsonData) {
